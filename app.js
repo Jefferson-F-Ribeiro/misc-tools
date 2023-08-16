776295d1fa777f88916613b1d546a3f4307574af
Jefferson-F-Ribeiro <93939600+Jefferson-F-Ribeiro@users.noreply.github.com>
2023-08-16 17:41:10 -0300

    Adiciona rotas iniciais

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello, World!\n');
+	if(req.url == '/'){
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+		res.end('Bem vindo ao meu aplicativo!\n');
+	}
+
+	else if(req.url == '/about'){
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+		res.end('Página sobre\n');	
+	}
+	
+	else {
+		res.statusCode = 404;
+		res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+		res.end('404 página não encontrada\n');
+	}
 });
 
 const port = 3000;
