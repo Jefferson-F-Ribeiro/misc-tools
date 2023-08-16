dbdafeb44537c9daef50e491e7cbc95d7cf55b51
Jefferson-F-Ribeiro <93939600+Jefferson-F-Ribeiro@users.noreply.github.com>
2023-08-16 20:44:54 -0300

    Adiciona links nas páginas

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const http = require('http');
 
+function generateHomePage(){
+	return `
+		<a href="/about">Ir para a Página Sobre</a><br>
+		Bem vindo ao meu aplicativo!
+		`;
+}
+
+function generateAboutPage(){
+	return `
+		<a href="/">Voltar para a Página Inicial</a><br>
+		Página Sobre
+		`;	
+}
+
 const server = http.createServer((req, res) => {
 	if(req.url == '/'){
 		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-		res.end('Bem vindo ao meu aplicativo!\n');
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end(generateHomePage());
 	}
 
 	else if(req.url == '/about'){
 		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-		res.end('Página sobre\n');	
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end(generateAboutPage());	
 	}
 	
 	else {
 		res.statusCode = 404;
-		res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+		res.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'});
 		res.end('404 página não encontrada\n');
 	}
 });
